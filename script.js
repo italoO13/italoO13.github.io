43047ed82db14a6edb5f4ea56d9b819fd49f934e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-01-27 15:59:23 -0300

    Selecionar imagem

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ opcao1.addEventListener("change",texto())
 
 docu.addEventListener("click",function(e){
     const texto = document.querySelector("#texto-informativo")
+    const foto = document.querySelector("#sobremim-img")
     let target = e.target;
     switch (target.id){
         case "opt1":
             texto.innerHTML= "Me chamo Italo, meu principal principal hobbie é comer (kkk) e gosto de fazer isso acompanhado."
+            foto.src = "./imagem/sushi.jpg"
             break;
+
         case "opt2":
             texto.innerHTML="Me chamo Italo, meu principal principal hobbie é comer (kkk) e gosto de fazer isso acompanhado. Ainda durante a graduação de curso finalmente entrei no mercado através de uma empresa jr de gestão e comecei a descobrir que minha missão como profissional é resolver problemas. )"
+            foto.src = "./imagem/ciclo.jpg"            
             break;
         case "opt3":
             texto.innerHTML= `
@@ -27,9 +31,11 @@ docu.addEventListener("click",function(e){
              acompanhado. Ainda durante a graduação de curso finalmente entrei no mercado através
              de uma empresa jr de gestão e comecei a descobrir que minha missão como profissional é
              resolver problemas. <br> <br>
-            Neste periodo que atuei no mercado, percebi a carência das empresa em conectar seus processos com o mundo da tecnologia,
+            Neste periodo que atuei no mercado, percebi a carência das empresa em conectar seus processos com o mundo da tecnologia,     
+            
             `
-
+            foto.src = "./imagem/eu.png" 
+            break;
 
     }
 
