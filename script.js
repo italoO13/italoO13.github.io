7c1bf4c725b779e061ace2b197018bbf2e4076eb
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-01-27 00:04:01 -0300

    criando funcionalidade de pouco/muito

---
+++ script.js
@@ -0,0 +1,73 @@
+var opcao1 = document.querySelector("#opt1")
+var opcao2 = document.querySelector("#opt2")
+var opcao3 = document.querySelector("#opt3")
+var opcao4 = document.querySelector("#opt4")
+var opcao5 = document.querySelector("#opt5")
+var opcao6 = document.querySelector("#opt6")
+var docu = document.querySelector("body")
+
+
+opcao1.addEventListener("change",texto())
+
+
+
+docu.addEventListener("click",function(e){
+    const texto = document.querySelector("#texto-informativo")
+    let target = e.target;
+    switch (target.id){
+        case "opt1":
+            texto.innerHTML= "Me chamo Italo, meu principal principal hobbie é comer (kkk) e gosto de fazer isso acompanhado."
+            break;
+        case "opt2":
+            texto.innerHTML="Me chamo Italo, meu principal principal hobbie é comer (kkk) e gosto de fazer isso acompanhado. Ainda durante a graduação de curso finalmente entrei no mercado através de uma empresa jr de gestão e comecei a descobrir que minha missão como profissional é resolver problemas. )"
+            break;
+        case "opt3":
+            texto.innerHTML= `
+            Me chamo Italo, meu principal principal hobbie é comer (kkk) e gosto de fazer isso
+             acompanhado. Ainda durante a graduação de curso finalmente entrei no mercado através
+             de uma empresa jr de gestão e comecei a descobrir que minha missão como profissional é
+             resolver problemas. <br> <br>
+            Neste periodo que atuei no mercado, percebi a carência das empresa em conectar seus processos com o mundo da tecnologia,
+            `
+
+
+    }
+
+
+
+})
+
+
+function texto(){
+
+    console.log(this.value)
+}
+
+
+
+
+
+
+
+
+
+/*
+function texto(){
+    const tex = document.querySelector("#texto-informativo")
+
+    for (var j = 0; j<opcao.length;j++){
+        if (opcao[j].checked===true){
+            console.log(opcao[j])
+        }
+    }
+}
+
+for(var i =0 ; i < opcao.length;i++){
+        opcao[i].addEventListener("click",function(op){
+            if (op.value ==="2"){
+                console.log("verdadeiro")
+            }
+        
+        })
+    }
+*/
